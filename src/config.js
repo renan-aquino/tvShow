af2f9c904ad629a2e5430f9e34255b12a4167500
Renan-Feliz <renan.aquino99@gmail.com>
2023-05-11 00:24:29 -0300

    request fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://api.themoviedb.org/3/";
+const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY_PARAM = "?api_key=ca9d374b357e87838eecdd423851e399";
 const ORIGINAL_LANGUAGE = "&with_original_language=es|pt|en|fr|kr|jp|de|it|ru"
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original"
